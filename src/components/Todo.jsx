f539c8af1d6e792009da703fcea8d09f740dd18a
PrincxDevitt <PrincxDevitt@gmail.com>
2025-01-18 10:16:05 -0500

    Agregar nueva tarea a la lista
    Se creo la funcionalidad para agregar tareas a la lista

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,45 +1,66 @@
-import React, { useRef } from 'react'
-import Todo_icon from '../assets/Todo_icon.png'
-import TodoItems from './TodoItems'
+import React, { useRef, useState } from 'react';
+import Todo_icon from '../assets/Todo_icon.png';
+import TodoItems from './TodoItems';
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
 
-const inputRef = useRef();
+  const inputRef = useRef();
 
-const add=()=>{
-    const inputText = inputRef.current.value;
-    console.log(inputText);
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
 
-}
+    if (inputText === "") {
+      return null;
+    }
 
- return (
-    <div className="bg-white place-self-center w-11/12 max-wmd
-    flex flex-col p-7 min-h-[500px] rounded-x1">
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
 
-      {/*Title*/ }
-      <div className='flex items-center mt-7 gap-2'>
-        <img className='w-8' src={Todo_icon} alt="" />
-        <h1 className='text-3x1 font-semibold'>To-Do List</h1>
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+
+  return (
+    <div className="bg-white place-self-center w-11/12 max-w-md
+    flex flex-col p-7 min-h-[500px] rounded-xl">
+      {/* Title */}
+      <div className="flex items-center mt-7 gap-2">
+        <img className="w-8" src={Todo_icon} alt="Todo Icon" />
+        <h1 className="text-3xl font-semibold">To-Do List</h1>
       </div>
-      
-      {/*input box*/ }
-      <div className=' flex items-center my-7 bg-gray-200
-      rounded-full'>
-        <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2
-         placeholder:text-slate-600' type="text" placeholder='Add Your List'/>
-        <button onClick={add} className='border-none rounded-full
-        bg-green-600 w-32 h-14 text-white text-lg font-medium
-        cursor-pointer'> ADD +</button>
+
+      {/* Input box */}
+      <div className="flex items-center my-7 bg-gray-200
+      rounded-full">
+        <input
+          ref={inputRef}
+          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2
+          placeholder:text-slate-600"
+          type="text"
+          placeholder="Add Your List"
+        />
+        <button
+          onClick={add}
+          className="border-none rounded-full
+          bg-green-600 w-32 h-14 text-white text-lg font-medium
+          cursor-pointer"
+        >
+          ADD +
+        </button>
       </div>
 
-       {/*todo list*/ }
-       <div>
-        <TodoItems text="Learn Coding"/>
-        <TodoItems text="Learn Coding From Great"/>
-       </div>
-      
+      {/* Todo list */}
+      <div>
+        {todoList.map((item) => (
+          <TodoItems key={item.id} text={item.text} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
